c868ee96d71f80040b290b90d0c8f2b622d6c62c
Shin MG <shinmg2520@gmail.com>
2017-11-26 21:05:35 +0000

    solved 10172

---
+++ 10172/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+    cout << a;
+    return 0;
+}
