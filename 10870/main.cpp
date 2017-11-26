da14b9d0c484c624b1576677ddd01ac143355837
Shin MG <shinmg2520@gmail.com>
2017-11-26 23:20:25 +0000

    solved 10870

---
+++ 10870/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<double> fib;
+    cin >> n;
+    fib.reserve(n+1);
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i=2; i<=n; i++)
+        fib[i] = fib[i-1] + fib[i-2];
+    cout << fib[n];
+}
