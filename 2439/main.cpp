46c7b213b82fd0993b36f69b2627ca8f629a3561
Shin MG <shinmg2520@gmail.com>
2017-11-27 08:03:04 +0000

    solved 2438,2439

---
+++ 2439/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i,j;
+    cin >> n;
+    for ( i=0 ; i<n; i++){
+        for(j=0;j<n-i-1;j++)
+            cout << " ";
+        for (j=0; j< i+1; j++)
+            cout << "*";
+        cout << "\n";
+    }
+}
