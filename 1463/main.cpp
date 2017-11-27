2f9857578ae5c7aeaa1f3de1639cb36102194593
Shin MG <shinmg2520@gmail.com>
2017-11-27 11:19:10 +0000

    solved 1463

--- 1463/main.cpp
+++ 1463/main.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main()
 {
-    int n;
-    
+    int n, i;
+    vector<int> arr;
+    cin >> n;
+    arr.reserve(n+1);
+    arr[1]=0;
+    for (i=2;i<=n;i++){
+        if(i%3 == 0)
+            arr[i] = min(arr[i/3], arr[i-1])+1;
+        else if(i%2 ==0)
+            arr[i] = min(arr[i/2], arr[i-1])+1;
+        else
+            arr[i] = arr[i-1]+1;
+    }
+    cout << arr[n];
+    return 0;
 }
