e16352d66bb14a07ac61c092112d06189a37ed15
Shin MG <shinmg2520@gmail.com>
2017-11-27 16:14:14 +0000

    solved 1009

--- 1009/main.cpp
+++ 1009/main.cpp
@@ -13,28 +13,35 @@ int getLen(int a){
     return cnt;
 }
 
-int main()
+void printResult(int a, int b)
 {
-    int a,b,c,len,i;
-    cin >> a >> b >> c;
+    int len,i;
     len = getLen(b);
     vector<long long> arr;
     arr.reserve(len);
     string bin = bitset<32>(b).to_string();
     bin = bin.substr(32-len);
-    arr.push_back(a%c);
+    arr.push_back(a%10);
     for(i=1;i<len;i++)
-        arr.push_back(((arr.back()%c)*(arr.back()%c))%c);
-        
-    // for(i=0;i<len;i++)
-    //     cout << i << " : " << arr[i] << endl;
-    // cout << endl << bin << endl;    
-        
+        arr.push_back((arr.back()*arr.back())%10);
+    
     int result = 1;    
     for(i=0;i<len;i++)
         if(bin[i] == '1')
-            result = result*arr[len-i-1] % c;
-    
-    cout << result;
+            result = result*arr[len-i-1] % 10;
+    if (result == 0)
+        cout << 10 << endl;
+    else
+        cout << result << endl;
+}
+
+int main()
+{
+    int n,a,b;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        printResult(a,b);
+    }
     return 0;
 }
