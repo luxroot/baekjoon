d92eb0a14f0abd7aefc2e9fac2fae5ec66db2cf3
Shin MG <shinmg2520@gmail.com>
2017-11-28 09:49:54 +0000

    solved 1024

---
+++ 1024/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n,l,i;
+    cin >> n >> l;
+    i=l;
+    for(;;i++){
+        if(i > 100 || (n-(i*(i-1)/2)) < 0){
+            cout << -1 << endl;
+            exit(0);
+        }
+        
+        if( (n-(i*(i-1)/2)) % i == 0)
+            break;
+    }
+    int start = (n-(i*(i-1)/2)) / i;
+    for(int k=0;k<i;k++)
+        cout << start+k << " ";
+    return 0;
+}
