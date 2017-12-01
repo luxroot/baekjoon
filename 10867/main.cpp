bcc07bcace1349f2070abe0f54f1ffd61399e8c3
Shin MG <shinmg2520@gmail.com>
2017-12-01 20:34:21 +0000

    solved 10867

--- 10867/main.cpp
+++ 10867/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 using namespace std;
-bool arr[100100];
+bool arr[200100];
 
 int main(){
     int n,i,temp;
     cin >> n;
     for(i=0;i<n;i++){
         cin >> temp;
-        arr[temp]=true;
+        arr[temp+100000]=true;
     }
-    for(i=0;i<100000;i++)
+    for(i=0;i<200010;i++)
         if(arr[i])
-            printf("%d ",i);
+            printf("%d ",i-100000);
     return 0;
 }
