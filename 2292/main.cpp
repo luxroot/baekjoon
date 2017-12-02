4a19765d7188ae6a605e821cb991487ffd2c6b7c
Shin MG <shinmg2520@gmail.com>
2017-12-02 07:36:14 +0000

    solved

---
+++ 2292/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cin >> n;
+    j=1;
+    for(i=1;;i++){
+        if(n<=j)
+            break;
+        j+=6*i;
+    }
+    cout << i;
+}
