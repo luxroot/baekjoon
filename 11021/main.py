612dcefd8e2d374fc640426fdb1febf62342121c
Shin MG <shinmg2520@gmail.com>
2017-12-12 19:31:20 +0000

    solved add

---
+++ 11021/main.py
@@ -0,0 +1,4 @@
+n=input()
+for i in range(n):
+    (a,b) = map(int,raw_input().split())
+    print "Case #"+str(i+1)+": "+str(a+b)
---
+++ 11022/main.py
@@ -0,0 +1,4 @@
+n=input()
+for i in range(n):
+    (a,b) = map(int,raw_input().split())
+    print "Case #"+str(i+1)+": "+str(a)+" + "+str(b)+" = "+str(a+b)
