d5a289b2352e6fa3d42d5d79dab9baec50ab070f
Shin MG <shinmg2520@gmail.com>
2017-12-14 03:57:23 +0000

    solved 2941

--- 2941/main.py
+++ 2941/main.py
@@ -2,10 +2,10 @@ a=raw_input()
 l=len(a)
 l-=a.count('c=')
 l-=a.count('c-')
-l-=a.count('dj=')
+l-=(a.count('z=')-a.count('dz='))
+l-=(a.count('dz='))*2
 l-=a.count('d-')
 l-=a.count('lj')
 l-=a.count('nj')
 l-=a.count('s=')
-l-=a.count('z=')
 print l
