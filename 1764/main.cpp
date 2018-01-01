47ee19480cbbd38b1edba96b7d10865741b0e915
Shin MG <shinmg2520@gmail.com>
2018-01-01 11:10:56 +0000

    solved 1764

--- 1764/main.cpp
+++ 1764/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+#include <map>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int n,m.i;
-    vector<string> first;
-    vector<string> last;
+    int n,m,i;
+    char asdf[30];
+    map<string,bool> first;
+    map<string,bool>::iterator iter;
+    vector<string> result;
     string temp;
     cin >> n >> m;
     for(i=0;i<n;i++)
     {
-        cin >> temp;
-        first.push_back(temp);
+        scanf("%s",asdf);
+        first.insert(pair<string,bool>(string(asdf),true));
     }
     for(i=0;i<m;i++){
-        
+        scanf("%s",asdf);
+        temp=string(asdf);
+        iter=first.find(temp);
+        if ( iter != first.end() )
+            result.push_back(temp);
     }
+    sort(result.begin(),result.end());
+    printf("%d\n",(int)result.size());
+    for(i=0;i<result.size();i++)
+        printf("%s\n",result[i].c_str());
+    return 0;
 }
