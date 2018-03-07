7ea4cbcb61ccc134bf84b489f5a8fb169f2491e2
Shin MG <shinmg2520@gmail.com>
2018-03-07 07:37:22 +0000

    solved

--- 15494/main.cpp
+++ 15494/main.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main()
 {
-    int i,a,b;
+    int i,a,b=1;
     cin >> a;
-    a=a/364;
-    if(a>100){
-        cout << a-3*(a-99)/3 << endl << (a-98)/3;
-        return 0;
+    a=a/364-3;
+    while(a>100){
+        a-=3;
+        b++;
     }
-    a-=3;
-    cout << a << endl << 1;
+    cout << a << endl << b;
     return 0;
 }
