c80c82be5ec5f556b649efd1b5d8a8a67b3aa5d2
Shin MG <shinmg2520@gmail.com>
2018-03-19 00:14:12 +0000

    solve

--- 11726/dp.cpp
+++ 11726/dp.cpp
@@ -9,7 +9,7 @@ int main()
     arr[1]=1;
     cin >> n;
     for(i=2;i<=n;i++){
-        arr[i]=arr[i-1]+arr[i-2];
+        arr[i]=(arr[i-1]+arr[i-2])%10007;
     }
     cout << arr[n];
     return 0;
