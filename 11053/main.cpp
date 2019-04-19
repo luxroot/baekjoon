b3303f699ecefa1cb0133cc7e9f2f691e6a49b05
Shin MG <shinmg2520@gmail.com>
2019-04-19 13:48:04 +0000

    updated

--- 11053/main.cpp
+++ 11053/main.cpp
@@ -4,27 +4,21 @@ using namespace std;
 
 int main(){
     int arr[1010];
-    int dp[1010][1010];
-    int n,i,j,k,mm=0;
+    int dp[1010]={0,};
+    int n,i,j;
     ios::sync_with_stdio(false);
     cin >> n;
     for(i=0;i<n;i++){
         cin >> arr[i];
     }
-    for(i=0;i<n;i++){
-        dp[i][i]=1;
-        for(j=i+1;j<n;j++){
-            for(k=i;k<j;k++){
-                if(arr[k]<arr[j] && dp[i][k]+1 > dp[i][j])
-                    dp[i][j]=dp[i][k]+1;
-            }
+    dp[0]=1;
+    for(i=1;i<n;i++){
+        dp[i]=1;
+        for(j=0;j<i;j++){
+            if(arr[j]<arr[i] && dp[j]+1 > dp[i])
+                dp[i]=dp[j]+1;
         }
     }
-    for(i=0;i<n;i++){
-        for(j=i;j<n;j++){
-            mm=max(mm,dp[i][j]);
-        }
-    }
-    cout << mm;
+    cout << *max_element(dp,dp+n);
     return 0;
 }
