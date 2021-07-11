a26c5aea0f5322c60977a536a117a505a98146ed
MyeongGeun Shin <mg.shin@kaist.ac.kr>
2021-07-11 19:40:14 +0900

    Solved

--- 11052/dp.cpp
+++ 11052/dp.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    int n,i,j,arr[1010],dp[1010]={0};
-    cin >> n;
-    arr[0]=0;
-    for(i=0;i<n;i++){
-        cin >> arr[i+1];
-        dp[i]=0;
+int main() {
+  int n, i;
+  vector<int> arr, dp;
+  cin >> n;
+  arr.resize(n+1);
+  dp.resize(n+1);
+  for(i = 0; i < n; i++) {
+    cin >> arr.at(i+1);
+  }
+  for(i=0; i<=n; i++) {
+    for(int j = 1; j <= arr.size(); j++) {
+      if (i+j > n) continue;
+      dp.at(i+j) = max(dp.at(i+j), dp.at(i) + arr.at(j));
     }
-    for(i=1;i<=n;i++){
-        for(j=1;j<=i;j++){
-            dp[i]=max(dp[i],dp[i-j]+arr[j]);
-        }
-    }
-    // for(i=1;i<=n;i++)
-    //     cout << i << ' ' << dp[i] << endl;
-    cout << dp[n];
-    return 0;
+  }
+  cout << dp.back() << endl;
+  return 0;
 }
